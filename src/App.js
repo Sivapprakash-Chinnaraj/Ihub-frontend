154c1dda62337b4fa2102825e711dd06c4117eaa
santhosh24082003 <chendrayasanthosh05@gmail.com>
2024-08-12 11:40:25 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello world 
+          hello wo
         </p>
         <a
           className="App-link"
